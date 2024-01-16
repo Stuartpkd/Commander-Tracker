105c6f63c353de1e9b126fd18fa519b422dead87
stuartpkd <stuartparkerdesign@gmail.com>
2024-01-16 15:47:55 +0000

    Create card model in home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=200)
+    mana_cost = models.CharField(max_length=50, blank=True)
+    cmc = models.FloatField(default=0)  # Converted Mana Cost
+    type_line = models.CharField(max_length=200)
+    oracle_text = models.TextField(blank=True)
+    colors = models.CharField(max_length=50, blank=True)
+    image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
