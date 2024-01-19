592917475e7d26631e76cb3c7e979257c40ac8c3
stuartpkd <stuartparkerdesign@gmail.com>
2024-01-19 14:58:46 +0000

    Create card detail view

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from home.models import Card
 
-# Create your views here.
+
+class CardDetailView(DetailView):
+    model = Card
+    template_name = 'home/card_detail.html'  # Replace with your template path
+    context_object_name = 'card'
