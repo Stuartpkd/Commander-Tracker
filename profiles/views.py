8a0ab87fec866a841fe6e8c71ca8386e8e36c7e2
stuartpkd <stuartparkerdesign@gmail.com>
2024-01-19 14:40:01 +0000

    Create view for rendering the profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def user_profile(request):
+
+    context = {'user': request.user}
+    return render(request, 'profiles/templates/profile.html', context)
