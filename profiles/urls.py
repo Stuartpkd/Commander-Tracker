2e58e2a4b0225ba057dfd15e13186b4fdae1d5dd
stuartpkd <stuartparkerdesign@gmail.com>
2024-01-22 16:36:31 +0000

    Create url for creating categories on profile page

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_profile, add_card_to_category
+from .views import user_profile, add_card_to_category, create_category
 
 urlpatterns = [
     path('profile/', user_profile, name='user_profile'),
     path('add_card_to_category/<int:card_id>/', add_card_to_category, name='add_card_to_category'),
+    path('create_category/', create_category, name='create_category'),
 ]
