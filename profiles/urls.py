d89082e50768916efc316c685b5e12e226356c57
stuartpkd <stuartparkerdesign@gmail.com>
2024-01-22 16:33:18 +0000

    Add url for add card to category view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import user_profile
+from .views import user_profile, add_card_to_category
 
 urlpatterns = [
     path('profile/', user_profile, name='user_profile'),
+    path('add_card_to_category/<int:card_id>/', add_card_to_category, name='add_card_to_category'),
 ]
