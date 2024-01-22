89cb9f26afe02fe92c029ebc874fd5e8cc4e6bd1
stuartpkd <stuartparkerdesign@gmail.com>
2024-01-22 16:07:16 +0000

    Create view for making commander categories

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Category
 
 
 @login_required
@@ -7,3 +8,12 @@ def user_profile(request):
 
     context = {'user': request.user}
     return render(request, 'profiles/profiles.html', context)
+
+
+def create_category(request):
+    if request.method == 'POST':
+        category_name = request.POST.get('category_name')
+        Category.objects.create(name=category_name, user=request.user)
+        return redirect('profile_page')  # Redirect to the profile page or category management page
+
+    return render(request, 'profiles/create_category.html')
